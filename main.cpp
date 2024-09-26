86e2a1a2d4fe10bd1c6560b459fe5c27ece4db97
shuhaibw <120868621+shuhaibw@users.noreply.github.com>
2024-09-25 20:55:28 -0700

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
+// COMSC-210 | Lab 9 | Shuhaib Walahi
+
 #include <iostream>
+#include <array>
+
 using namespace std;
 
-int main() {
-    // complete this code in order to make it output
-    // the numbers 1-100
+const int SIZE = 100;
 
-    for ()
-        cout << i << " ";
+int main() {
+
+    // array to hold the numbers
+    array<int,SIZE> numbers;
+
+    // populate array with 1-100
+    for (int i = 0; i < SIZE; i++)
+    {
+        numbers[i] = i + 1;
+    }
+
+    // output values seperated by commas
+    for (int val: numbers)
+    {
+        cout << val;
+        if (val < SIZE)
+        {
+            cout << ", ";
+        }
+    }
 
     return 0;
 }
